c658fcd0091a7430c4485bf3e94a1f7e5ad91e7e
Nikita Lebedev <eazy-f@yandex.ru>
2016-10-31 22:48:09 +0000

    Parse basic data types
    * strings
    * structures
    * lists

--- src/claude_generator.erl
+++ src/claude_generator.erl
@@ -55,12 +55,38 @@ camel_parts([Capital | Rest], Acc) when ?is_capital(Capital) ->
     {WordTail, Tail} = lists:splitwith(fun(C) -> not ?is_capital(C) end, Rest),
     camel_parts(Tail, [[string:to_lower(Capital) | WordTail] | Acc]).
 
+http_method(Method) when Method == <<"POST">>;
+                         Method == <<"GET">>;
+                         Method == <<"DELETE">>;
+                         Method == <<"PUT">> ->
+    binary_to_atom(Method, latin1).
+
 operation_function(Operation, Shapes) ->
-    #{<<"name">> := Name} = Operation,
     Output = maps:get(<<"output">>, Operation, undefined),
-    FunctionBody = [erl_syntax:abstract(Output)],
+    #{<<"name">> := Name,
+      <<"input">> := InputShape,
+      <<"http">> := #{
+        <<"method">> := Method,
+        <<"requestUri">> := Uri
+    }} = Operation,
     FunctionName = erl_syntax:atom(list_to_atom(claude_name(Name))),
     Args = [erl_syntax:variable('Client'), erl_syntax:variable('Parameters')],
+    CallClient = erl_syntax:application(erl_syntax:atom(claude_client),
+                                        erl_syntax:atom(request),
+                                        [erl_syntax:abstract(http_method(Method)),
+                                         erl_syntax:abstract(Uri),
+                                         erl_syntax:variable('Client'),
+                                         erl_syntax:variable('ParsedParameters')]),
+    ParseParameters = parameters_parser(InputShape, Shapes),
+    FunctionBody = [erl_syntax:match_expr(
+                      erl_syntax:variable('Parser'),
+                      ParseParameters),
+                    erl_syntax:match_expr(
+                      erl_syntax:variable('ParsedParameters'),
+                      erl_syntax:application(
+                       erl_syntax:variable('Parser'),
+                       [erl_syntax:variable('Parameters')])),
+                    CallClient],
     erl_syntax:function(FunctionName, [erl_syntax:clause(Args, none, FunctionBody)]).
     
 exports(ExportedFunctions) ->
@@ -69,3 +95,101 @@ exports(ExportedFunctions) ->
                   || Fun <- ExportedFunctions],
     erl_syntax:attribute(erl_syntax:atom(export),
                          [erl_syntax:list(ExportList)]).
+
+parameters_parser(Definition, Shapes) ->
+    #{<<"shape">> := ShapeName} = Definition,
+    {ok, Shape} = maps:find(ShapeName, Shapes),
+    #{<<"type">> := ShapeType} = Shape,
+    EntryFail = fail_clause({unexpected_type, ShapeType}),
+    case ShapeType of
+        <<"structure">> ->
+            #{<<"members">> := Members} = Shape,
+            Clauses = [parameter_parser_clause(Name, MemberDefinition, Shapes)
+                        || {Name, MemberDefinition} <- maps:to_list(Members)],
+            VarStruct = prefix_var("Struct", binary_to_list(ShapeName)),
+            EntryClause = erl_syntax:clause(
+                            [erl_syntax:variable(VarStruct)],
+                            erl_syntax:application(
+                             erl_syntax:atom(is_map),
+                             [erl_syntax:variable(VarStruct)]),
+                            [erl_syntax:match_expr(
+                               erl_syntax:variable('Parser'),
+                               erl_syntax:fun_expr(Clauses)),
+                             erl_syntax:application(
+                              erl_syntax:atom(maps),
+                              erl_syntax:atom(fold),
+                              [erl_syntax:variable('Parser'),
+                               erl_syntax:abstract(#{}),
+                               erl_syntax:variable(VarStruct)]
+                              )]),
+            Entry = erl_syntax:fun_expr([EntryClause, EntryFail]),
+            Entry;
+        <<"list">> ->
+            #{<<"member">> := MemberDefinition} = Shape,
+            VarList = prefix_var("List", binary_to_list(ShapeName)),
+            EntryClause = erl_syntax:clause(
+                            [erl_syntax:variable(VarList)],
+                            erl_syntax:application(
+                             erl_syntax:atom(is_list),
+                             [erl_syntax:variable(VarList)]),
+                            [erl_syntax:application(
+                              erl_syntax:atom(lists),
+                              erl_syntax:atom(map),
+                              [parameters_parser(MemberDefinition, Shapes),
+                               erl_syntax:variable(VarList)])]),
+            erl_syntax:fun_expr([EntryClause, EntryFail]);
+        <<"string">> ->
+            VarString = prefix_var("String", binary_to_list(ShapeName)),
+            EntryClause = erl_syntax:clause(
+                            [erl_syntax:variable(VarString)],
+                            erl_syntax:application(
+                             erl_syntax:atom(is_list),
+                             [erl_syntax:variable(VarString)]),
+                            [return_var(VarString)]),
+            erl_syntax:fun_expr([EntryClause, EntryFail]);
+        _ ->
+            erl_syntax:fun_expr(
+              [erl_syntax:clause(
+                [erl_syntax:variable('_')],
+                none,
+                [erl_syntax:abstract({value, "undefined"})]),
+              fail_clause({unexpected_type, ShapeType})])
+    end.
+
+return_var(VarName) ->
+    erl_syntax:tuple([erl_syntax:atom(value), erl_syntax:variable(VarName)]).
+
+prefix_var(Name, Prefix) ->
+    string:concat(Name, Prefix).
+
+fail_clause(Reason) ->
+    erl_syntax:clause(
+      [erl_syntax:variable('_')],
+      none,
+      [erl_syntax:application(
+         erl_syntax:atom(throw),
+         [erl_syntax:abstract(Reason)])]).
+
+parameter_parser_clause(Name, Definition, Shapes) ->
+    Parser = parameters_parser(Definition, Shapes),
+    [Clause, _FailClause] = erl_syntax:fun_expr_clauses(Parser),
+    [ValuePattern] = erl_syntax:clause_patterns(Clause),
+    Key = struct_member_key(Name, Definition),
+    erl_syntax:clause(
+      [erl_syntax:abstract(list_to_atom(claude_name(Name))),
+       ValuePattern,
+       erl_syntax:variable('Acc')],
+      erl_syntax:clause_guard(Clause),
+      [erl_syntax:match_expr(
+        erl_syntax:variable('Res'),
+        erl_syntax:block_expr(erl_syntax:clause_body(Clause))),
+       erl_syntax:map_expr(
+         erl_syntax:variable('Acc'),
+         [erl_syntax:map_field_assoc(
+           erl_syntax:abstract(Key),
+           erl_syntax:variable('Res'))])]).
+
+struct_member_key(_OriginalName, #{<<"locationName">> := LocationName}) ->
+    binary_to_list(LocationName);
+struct_member_key(OriginalName, _Definition) ->
+    binary_to_list(OriginalName).
